22ede9d612b2516d9fd58a3773ec8d0da724d428
Sarah Morris <bubblegumquasar@gmail.com>
2017-01-26 16:47:53 -0600

    building forms. They are on the page, but don't do much yet

--- main.py
+++ main.py
@@ -16,10 +16,76 @@
 #
 import webapp2
 
-class MainHandler(webapp2.RequestHandler):
+# html boilerplate for the top of every page
+page_header = """
+<!DOCTYPE html>
+<html>
+<head>
+    <title>User Signup</title>
+    <style type="text/css">
+        .error {
+            color: red;
+        }
+    </style>
+</head>
+<body>
+    <h1>
+        User Signup
+    </h1>
+"""
+
+# html boilerplate for the bottom of every page
+page_footer = """
+</body>
+</html>
+"""
+
+#setup for main page
+class Index(webapp2.RequestHandler):
     def get(self):
-        self.response.write('Hello world!')
+        #Username form
+        user_username = """
+        <form action="/username" method="post">
+            <label>
+                Username:
+                <input type="text" name="username"/>
+            </label>
+        </form>
+        """
+
+        #password setup
+        user_password = """
+        <form action="/password" method="post">
+            <label>
+                Password:
+                <input type="password" name="password"/>
+            </label>
+        </form>
+        """
+        #password verify
+        user_password_verify = """
+        <form action="/verify" method="post">
+            <label>
+                Password:
+                <input type="password" name="user_password_verify"/>
+            </label>
+        </form>
+        """
+        #email setup
+        user_email = """
+        <form action="/email" method="post">
+            <label>
+                Email (optional):
+                <input type="text" name="user_email"/>
+            </label>
+        </form>
+        """
+        
+        main_content = user_username + user_password + user_password_verify + user_email
+        content = page_header + main_content + page_footer
+
+        self.response.write(content)
 
 app = webapp2.WSGIApplication([
-    ('/', MainHandler)
+    ('/', Index)
 ], debug=True)
